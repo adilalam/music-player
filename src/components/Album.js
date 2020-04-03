97d3b82c9625077533c90be0a9b4f2be057b7600
Jessica Shepherd <jessicajadecodes@gmail.com>
2020-04-03 13:56:04 -0700

    player bar buttons complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import albumData from "./../data/albums";
+import PlayerBar from "./PlayerBar";
 
 class Album extends Component {
     constructor(props) {
@@ -45,6 +46,29 @@ class Album extends Component {
             }
             this.play();
         }
+    }
+
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(
+            song => this.state.currentSong === song
+        );
+        const newIndex = Math.max(0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
+    }
+
+    handleNextClick() {
+        const currentIndex = this.state.album.songs.findIndex(
+            song => this.state.currentSong === song
+        );
+        const newIndex = Math.min(
+            currentIndex + 1,
+            this.state.album.songs.length - 1
+        );
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
     }
 
     displayIndex(song, index) {
@@ -98,6 +122,15 @@ class Album extends Component {
                         ))}
                     </tbody>
                 </table>
+                <PlayerBar
+                    isPlaying={this.state.isPlaying}
+                    currentSong={this.state.currentSong}
+                    handleSongClick={() =>
+                        this.handleSongClick(this.state.currentSong)
+                    }
+                    handlePrevClick={() => this.handlePrevClick()}
+                    handleNextClick={() => this.handleNextClick()}
+                />
             </section>
         );
     }
