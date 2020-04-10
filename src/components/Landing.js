d5b055caf3a2928b9176e01430a8ec9efbb697b5
Jessica Shepherd <jessicajadecodes@gmail.com>
2020-04-10 16:49:31 -0700

    landing page styling complete

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faMobileAlt,
+    faMusic,
+    faHeadphones,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Landing = () => (
     <section className="landing">
@@ -9,6 +15,7 @@ const Landing = () => (
 
         <section className="selling-points">
             <div className="point">
+                <FontAwesomeIcon icon={faMusic} size="5x" className="icon" />
                 <h2 className="point-title">Choose your music</h2>
                 <p className="point-description">
                     The world is full of music - why should you have to listen
@@ -16,7 +23,11 @@ const Landing = () => (
                 </p>
             </div>
             <div className="point">
-                <span className="ion-headset"></span>
+                <FontAwesomeIcon
+                    className="icon"
+                    icon={faHeadphones}
+                    size="5x"
+                />
                 <h2 className="point-title">Unlimited, streaming, ad-free</h2>
 
                 <p className="point-description">
@@ -24,6 +35,11 @@ const Landing = () => (
                 </p>
             </div>
             <div className="point">
+                <FontAwesomeIcon
+                    className="icon"
+                    icon={faMobileAlt}
+                    size="5x"
+                />
                 <h2 className="point-title">Mobile enabled</h2>
                 <p className="point-description">
                     Listen to your music on the go. This streaming service is
