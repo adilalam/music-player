c3494aa62ef2da3b420cbaf13b68c32d21584981
Jessica Shepherd <jessicajadecodes@gmail.com>
2020-04-14 14:31:40 -0700

    library page styling complete

--- src/components/Library.js
+++ src/components/Library.js
@@ -12,16 +12,27 @@ class Library extends Component {
 
     render() {
         return (
-            <section className="library">
-                {this.state.albums.map((album, index) => (
-                    <Link to={`/album/${album.slug}`} key={index}>
-                        <img src={album.albumCover} alt={album.title} />
-                        <div>{album.title}</div>
-                        <div>{album.artist}</div>
-                        <div>{album.songs.length} songs</div>
-                    </Link>
-                ))}
-            </section>
+            <div>
+                <h2 className="point-title">Select an album</h2>
+                <section className="library">
+                    {this.state.albums.map((album, index) => (
+                        <Link
+                            to={`/album/${album.slug}`}
+                            key={index}
+                            className="libraryItem"
+                        >
+                            <img
+                                src={album.albumCover}
+                                alt={album.title}
+                                className="album-cover"
+                            />
+                            <div>{album.title}</div>
+                            <div>{album.artist}</div>
+                            <div>{album.songs.length} songs</div>
+                        </Link>
+                    ))}
+                </section>
+            </div>
         );
     }
 }
