9247d719e6d1f7e77a87dabdb914963e1c1f5c79
Jessica Shepherd <jessicajadecodes@gmail.com>
2020-04-17 15:54:01 -0700

    Updates

---
+++ precache-manifest.668fb324f20d9e77e11ace5fac59cc99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63bafb95a798e99e03fd8dbd96118e2",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "fa6ecbbbdf6c7cb344c6",
+    "url": "/music-player/static/css/main.0b725e83.chunk.css"
+  },
+  {
+    "revision": "f95919cdadef08fba085",
+    "url": "/music-player/static/js/2.01a4727b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/music-player/static/js/2.01a4727b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa6ecbbbdf6c7cb344c6",
+    "url": "/music-player/static/js/main.a9e80640.chunk.js"
+  },
+  {
+    "revision": "e7462f022b64bf5c7f6d",
+    "url": "/music-player/static/js/runtime-main.a7679282.js"
+  },
+  {
+    "revision": "9008fac87940cac34562011a1afb48a2",
+    "url": "/music-player/static/media/blur_bg_3.9008fac8.jpg"
+  },
+  {
+    "revision": "660b855a5d07aa5693d36ed79d996dce",
+    "url": "/music-player/static/media/hero-img.660b855a.jpg"
+  },
+  {
+    "revision": "71fd11a2eaebc38156fc7572ab16c2c0",
+    "url": "/music-player/static/media/music-jams-logo.71fd11a2.png"
+  }
+]);
